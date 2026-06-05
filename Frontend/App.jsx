431ee8126776f243a7b027dc9bbe3d3f5ef65994
spicy-programmer <tugudavid6@gmail.com>
2026-06-05 23:22:35 +0200

    first commit

---
+++ Frontend/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App(){
+    return (
+    <div className="first">
+        <header className="header">
+            <nav className="navbar">
+                <h1 className="logo">K-CAPITAL</h1>
+                <ul className="links"> 
+                    <li><a href="#home">Acceuil</a></li>
+                    <li><a href="#about">A propos</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+
+
+            </nav>
+        </header>
+        
+    </div>
+    );
+}
+export default App;
